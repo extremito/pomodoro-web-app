2af305af045ebc85098f01e7d870cadff39c6fa2
Mars <imarbeal@outlook.com>
2023-07-08 16:10:24 -0700

    Wait stage implemented in time display component

--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -6,16 +6,24 @@ interface TimeDisplay {
   timeset: number;
   displayPomodoro: boolean;
   displayRest: boolean;
+  displayWait: boolean;
   seconds?: number;
 }
 
-const getDisplayTitle = (pomodoroRunning: boolean, restRunning: boolean) => {
+const getDisplayTitle = (
+  pomodoroRunning: boolean,
+  restRunning: boolean,
+  displayWait: boolean
+) => {
   switch (true) {
     case pomodoroRunning: {
       return "Remaining pomodoro time";
     }
     case restRunning: {
       return "Remaining rest time";
+    }
+    case displayWait: {
+      return "Waiting for rest start";
     }
   }
 };
@@ -24,6 +32,7 @@ const TimeDisplay = ({
   timeset,
   displayPomodoro,
   displayRest,
+  displayWait,
   seconds = 0,
 }: TimeDisplay) => {
   const timeLeft = timeset - seconds;
@@ -34,7 +43,7 @@ const TimeDisplay = ({
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">
-          {getDisplayTitle(displayPomodoro, displayRest)}
+          {getDisplayTitle(displayPomodoro, displayRest, displayWait)}
         </h5>
         <p className="card-text">
           {minutes} : {formatInt(secondsLeft, 2)}
