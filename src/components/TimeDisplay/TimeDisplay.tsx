a6f6ebcb7983d61b7bd47ed51547d65349f11232
Mars <imarbeal@outlook.com>
2023-07-08 10:21:35 -0700

    Updated display title

--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -4,18 +4,38 @@ import formatInt from "../../utils/format/formatInt";
 
 interface TimeDisplay {
   timeset: number;
+  displayPomodoro: boolean;
+  displayRest: boolean;
   seconds?: number;
 }
 
-const TimeDisplay = ({ timeset, seconds = 0 }: TimeDisplay) => {
+const getDisplayTitle = (pomodoroRunning: boolean, restRunning: boolean) => {
+  switch (true) {
+    case pomodoroRunning: {
+      return "Remaining pomodoro time";
+    }
+    case restRunning: {
+      return "Remaining rest time";
+    }
+  }
+};
+
+const TimeDisplay = ({
+  timeset,
+  displayPomodoro,
+  displayRest,
+  seconds = 0,
+}: TimeDisplay) => {
   const timeLeft = timeset - seconds;
   const secondsLeft = timeLeft % 60;
   const minutes = Math.trunc(timeLeft / 60);
-  
+
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Elapsed time</h5>
+        <h5 className="card-title">
+          {getDisplayTitle(displayPomodoro, displayRest)}
+        </h5>
         <p className="card-text">
           {minutes} : {formatInt(secondsLeft, 2)}
         </p>
@@ -25,6 +45,9 @@ const TimeDisplay = ({ timeset, seconds = 0 }: TimeDisplay) => {
 };
 
 TimeDisplay.propTypes = {
+  timeset: PropTypes.number.isRequired,
+  displayPomodoro: PropTypes.bool.isRequired,
+  displayRest: PropTypes.bool.isRequired,
   seconds: PropTypes.number,
 };
 
--- src/components/TimeDisplay/test/TimeDisplay.test.tsx
+++ src/components/TimeDisplay/test/TimeDisplay.test.tsx
@@ -1,9 +1,40 @@
-import { render, screen } from "@testing-library/react"
-import TimeDisplay from "../TimeDisplay"
+import { render, screen } from "@testing-library/react";
+import TimeDisplay from "../TimeDisplay";
 
-describe('TimeDisplay component', () => {
-    test('should convert seconds to minutes', () => {
-        render(<TimeDisplay seconds={150} timeset={180}/>)
-        expect(screen.getByText('0 : 30'))
-    })
-})
+describe("TimeDisplay component", () => {
+  test("should convert seconds to minutes", () => {
+    render(
+      <TimeDisplay
+        seconds={150}
+        timeset={180}
+        displayPomodoro={true}
+        displayRest={false}
+      />
+    );
+    expect(screen.getByText("0 : 30")).toBeInTheDocument();
+  });
+
+  test("should display pomodoro title", () => {
+    render(
+      <TimeDisplay
+        seconds={150}
+        timeset={180}
+        displayPomodoro={true}
+        displayRest={false}
+      />
+    );
+    expect(screen.getByText("Remaining pomodoro time")).toBeInTheDocument();
+  });
+
+  test("should display pomodoro title", () => {
+    render(
+      <TimeDisplay
+        seconds={150}
+        timeset={180}
+        displayPomodoro={false}
+        displayRest={true}
+      />
+    );
+    expect(screen.getByText("Remaining rest time")).toBeInTheDocument();
+  });
+});
